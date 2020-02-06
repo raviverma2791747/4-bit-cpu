6c846dfa66aa40eaa424850dfea11c4ae7b701dc
raviverma2791747 <55842522+raviverma2791747@users.noreply.github.com>
2020-02-06 11:51:33 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -25,7 +25,9 @@ using namespace std;
 int clk=1;
 
 /*Main Memory*/
-int ram[16] = {ADD,01,01,0,OUT,3,HALT,EXIT};
+int ram[18] = {IN,0,IN,0,STORE,1,11,STORE,
+,12,ADD,0,0,0,OUT,13,HALT,EXIT};
+/*              0 1  2 3   4   5 6   7    8 9  10  111213 14 15 16  17
 int bus[4];
 
 /*Registers*/
@@ -35,12 +37,14 @@ int mar=0;
 int mdr=0;
 int acc=0;
 
+int add(int a,int b)
+{
+    int c;
+    return c = a+b;
+}
 int _ram(int addr,int choice)
 {
-    if(choice ==  ENABLE)
-    {
         return ram[addr];
-    }
 }
 
 void _ram(int addr,int choice,int data)
@@ -55,32 +59,47 @@ void instruction(int inst=0)
 {
    if(inst == ADD)
    {
+        _ram(pc + 3,SET,add(_ram(pc+1,ENABLE),_ram(pc+2,ENABLE)));
         pc = pc + 4;
-
-        _ram(pc - 1,SET,parallel_adder(_ram(pc-3,ENABLE),_ram(pc-2,ENABLE)));
    }
    else if(inst == OUT)
    {
+       cout<<_ram(_ram(pc + 1,ENABLE),ENABLE);
        pc = pc +2;
-       cout<<_ram(_ram(pc - 1,ENABLE),ENABLE);
    }
    else if(inst == HALT)
    {
+
 
        pc = pc + 1;
        system("pause");
 
    }
+   else if(inst == IN)
+   {
+       int temp=0;
+       cin>>temp;
+       _ram(pc+1,SET,temp);
+       pc =  pc + 2;
+   }
+   else if(inst == JUMP)
+   {
+     int temp = _ram(_ram(pc+1,ENABLE),ENABLE);
+     pc  = temp;
+   }
    else if(inst == EXIT)
    {
        clk = 0;
+       pc = pc + 1;
+   }
+   else if(inst == STORE)
+   {
+       _ram(_ram(pc+2,ENABLE),SET,_ram(pc+1,ENABLE));
+       pc =  pc + 3;
    }
 
 }
-void add()
-{
 
-}
 void alu()
 {
 
